65732e1c6d4e2f98e3fa83147ed94ba91b8b5a84
pedrootorres <phtg@cin.ufpe.br>
2016-03-19 00:11:37 -0300

    Clock added and working. Few code improvements

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
+var probs = 0;
 var frequency = 2000;
 var checkpoint = 10;
 var increment = 6;
 var increment2 = 18;
-var score = 0;
+var score = document.getElementById("score");
+var fakeScore = 0;
 var value = 0;
 var allMosquitos = 0;
 var playerStatus = document.getElementById("disease");
+var interval = setInterval(startGame, frequency);
+var clockProbability = 0.1;
+var clockEffect = false;
 
 function addMosquito() {
 	var x = Math.floor(Math.random() * 80);
@@ -22,20 +27,18 @@ function addMosquito() {
 
 function killMosquito(me) {
 	me.parentNode.removeChild(me);
+
 	score = document.getElementById("score");
 	value = parseInt(score.textContent);
 	value++;
 
+	if(!clockEffect) {
+		fakeScore++;
+	}
+
 	score.textContent = value;
-}
 
-function startGame() {
-	addMosquito();
-
-	score = document.getElementById("score");
-	value = parseInt(score.textContent);
-
-	if(value > checkpoint) {
+	if(fakeScore > checkpoint) {
 		checkpoint = 50/(10 - increment);
 		increment = increment + (10 - increment)/3;
 
@@ -54,6 +57,16 @@ function startGame() {
 		addTire();
 
 		interval = setInterval(startGame, frequency);
+	}
+}
+
+function startGame() {
+	addMosquito();
+
+	probs = Math.random();
+
+	if(probs < clockProbability) {
+		addClock();
 	}
 
 	allMosquitos = document.getElementsByClassName("aedes").length;
@@ -78,8 +91,6 @@ function startGame() {
 	}
 }
 
-var interval = setInterval(startGame, frequency);
-
 function restart() {
 	document.getElementById("blackCurtains").style.visibility = "hidden";
 	document.getElementById("over").style.visibility = "hidden";
@@ -98,8 +109,11 @@ function restart() {
 	increment = 6;
 	increment2 = 18;
 	score = 0;
+	fakeScore = 0;
 	value = 0;
 	allMosquitos = 0;
+	clockProbability = 0.1;
+	clockEffect = false;
 
 	interval = setInterval(startGame, frequency);			
 }
@@ -125,4 +139,30 @@ function gravity(obj, inter) {
 	if(y < 48) {
 		clearInterval(inter);
 	}
+}
+
+function addClock() {
+	var x = Math.floor(Math.random() * 80);
+	var y = Math.floor(Math.random() * 85);
+
+	var clock = document.createElement('div');
+	clock.setAttribute('class', 'clock');
+	clock.setAttribute('onclick', 'activateClock(this)');
+	clock.style.top = x + "%";
+	clock.style.left = y + "%";
+	document.getElementById("gameZone").appendChild(clock);
+
+	setTimeout(function() {clock.parentNode.removeChild(clock);}, 1500);
+}
+
+function activateClock(clock) {
+	clock.style.visibility = "hidden";
+	clockEffect = true;
+
+	clearInterval(interval);
+
+	setTimeout(function() {
+		clockEffect = false;
+		interval = setInterval(startGame, frequency);
+	}, 4000);
 }
