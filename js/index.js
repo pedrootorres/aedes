a9f85043851fb1046a9cd09aa617324e027b2c1a
pedrootorres <phtg@cin.ufpe.br>
2016-03-23 18:05:48 -0300

    General change. Mosquito frequency increases based on amount of trash

--- js/index.js
+++ js/index.js
@@ -1,18 +1,20 @@
 var gameZone = document.getElementById("gameZone");
-var probs = 0;
-var frequency = 2000;
-var checkpoint = 10;
-var increment = 6;
-var increment2 = 18;
 var score = document.getElementById("score");
-var fakeScore = 0;
-var value = 0;
-var allMosquitos = document.getElementsByClassName("aedes");
+var allTrash = document.getElementsByClassName("trash");
 var playerStatus = document.getElementById("disease");
-var interval;
+
+var mosquitoFrequency = 2000;
+var gameFrequency = 4000;
+
+var mainInterval;
+var mosquitoInterval;
+
+var probs = 0;
+var value = 0;
+var allMosquitos = 0;
 var clockProbability = 0.1;
 var clockEffect = false;
-var x, y;
+
 var mosquito;
 
 function addMosquito() {
@@ -25,76 +27,52 @@ function addMosquito() {
 	mosquito.style.top = x + "%";
 	mosquito.style.left = y + "%";
 
+	allMosquitos++;
 	gameZone.appendChild(mosquito);
-}
 
-function killMosquito(me) {
-	me.parentNode.removeChild(me);
-
-	value = parseInt(score.textContent);
-	value++;
-
-	if(!clockEffect) {
-		fakeScore++;
-	}
-
-	score.textContent = value;
-
-	if(fakeScore > checkpoint) {
-		checkpoint = 50/(10 - increment);
-		increment = increment + (10 - increment)/3;
-
-		frequency = 2000 - 1000/(20 - increment2);
-		increment2 = increment2 + (19.5 - increment2)/3;
-
-		clearInterval(interval);
-
-		if(frequency < 0) {
-			frequency = 100;
-		}
-
-		addTire();
-		addTire();
-		addTire();
-		addTire();
-
-		interval = setInterval(startGame, frequency);
-	}
-}
-
-function begin() {
-	interval = setInterval(startGame, frequency);
-
-	document.getElementById("start").style.visibility = "hidden";
-}
-
-function startGame() {
-	addMosquito();
-
-	probs = Math.random();
-
-	if(probs < clockProbability) {
-		addClock();
-	}
-
-	if(allMosquitos.length > 100) {
-		clearInterval(interval);
+	if(allMosquitos > 3) {
+		clearInterval(mainInterval);
+		clearInterval(mosquitoInterval);
 		document.getElementById("blackCurtains").style.visibility = "visible";
 		document.getElementById("finalScore").textContent = document.getElementById("score").textContent;
 		document.getElementById("over").style.visibility = "visible";
-	} else if(allMosquitos.length > 80) {
+	} else if(allMosquitos > 80) {
 		playerStatus.setAttribute('class', 'chikungunya');
 		playerStatus.textContent = "CHIKUNGUNYA";
-	} else if(allMosquitos.length > 40) {
+	} else if(allMosquitos > 40) {
 		playerStatus.setAttribute('class', 'zika');
 		playerStatus.textContent = "ZIKA";
-	} else if(allMosquitos.length > 20) {
+	} else if(allMosquitos > 20) {
 		playerStatus.setAttribute('class', 'dengue');
 		playerStatus.textContent = "DENGUE";
 	} else {
 		playerStatus.setAttribute('class', 'nada');
 		playerStatus.textContent = "NADA";
 	}
+}
+
+function killMosquito(me) {
+	me.parentNode.removeChild(me);
+	allMosquitos--;
+
+	value = parseInt(score.textContent);
+	value++;
+
+	score.textContent = value;
+}
+
+function begin() {
+	document.getElementById("start").style.visibility = "hidden";
+
+	mosquitoInterval = setInterval(addMosquito, mosquitoFrequency);
+	mainInterval = setInterval(startGame, 4000);
+}
+
+function startGame() {
+	addTrash();
+
+	clearInterval(mosquitoInterval);
+	mosquitoInterval = setInterval(addMosquito, mosquitoFrequency-(allTrash.length * 100));
 }
 
 function restart() {
@@ -104,37 +82,38 @@ function restart() {
 	playerStatus.setAttribute('class', 'nada');
 	playerStatus.textContent = "NADA";
 
-	allMosquitos = document.getElementById('gameZone');
-
-	while(allMosquitos.firstChild) {
-		allMosquitos.removeChild(allMosquitos.firstChild);
+	while(gameZone.firstChild) {
+		gameZone.removeChild(gameZone.firstChild);
 	}
 
-	frequency = 2000;
-	checkpoint = 10;
-	increment = 6;
-	increment2 = 18;
-	score = 0;
-	fakeScore = 0;
+	mosquitoFrequency = 2000;
+	gameFrequency = 4000;
+
+	probs = 0;
 	value = 0;
 	allMosquitos = 0;
 	clockProbability = 0.1;
 	clockEffect = false;
 
-	interval = setInterval(startGame, frequency);			
+	mosquitoInterval = setInterval(addMosquito, mosquitoFrequency);
+	mainInterval = setInterval(startGame, 4000);
 }
 
-function addTire() {
+function addTrash() {
 	var left = Math.floor(Math.random() * 85);
 
 	var tire = document.createElement('div');
-	tire.setAttribute('class', 'tire');
+	tire.setAttribute('class', 'trash tire');
 	tire.style.left = left + "%";
 	tire.style.bottom = Math.random() * (1000-900) + 900;
 
 	gameZone.appendChild(tire);
 
 	var inter = setInterval(function() {gravity(tire, inter);}, 25);
+}
+
+function destroyTrash() {
+
 }
 
 function gravity(obj, inter) {
