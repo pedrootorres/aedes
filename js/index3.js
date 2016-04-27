6ce258486de00c087df307c09925de2a4d8c93f0
pedrootorres <phtg@cin.ufpe.br>
2016-04-27 00:35:30 -0300

    Testing new random numbers generator

--- js/index3.js
+++ js/index3.js
@@ -1,5 +1,7 @@
 var gameZone = document.getElementById("gameZone");
 
+var random = new randomNumber();
+
 var mainInterval;
 
 var frame = -2000;
@@ -38,30 +40,44 @@ function updateGame() {
 }
 
 function addTrash() {
-	var whichTrash = Math.floor(Math.random() * 3);
-
-	var left = Math.floor(Math.random() * 85);
+	// var whichTrash = Math.floor(Math.random() * 3);
+	var whichTrash = Math.floor(random.get() * 3);
+	
+	// var left = Math.floor(Math.random() * 85);
+	var left = Math.floor(random.get() * (window.innerWidth-100));
 
 	var tire = document.createElement('div');
 	tire.setAttribute('class', 'trash ' + typesTrash[whichTrash]);
 	tire.setAttribute('life', "10");
 	tire.setAttribute('onclick', 'destroyTrash(this)');
-	tire.style.left = left + "%";
-	tire.style.bottom = Math.random() * (1000-900) + 900;
+	tire.style.left = left;
+	// tire.style.bottom = Math.random() * (1000-900) + 900;
+	tire.style.bottom = random.get() * (1000-900) + 900;
 
 	gameZone.appendChild(tire);
 	allTrash.push(tire);
 }
 
 function addMosquito() {
-	var x = Math.floor(Math.random() * (window.innerHeight-200 - 150) + 150);
-	var y = Math.floor(Math.random() * (window.innerWidth-100));
+	// var x = Math.floor(Math.random() * (window.innerHeight-200 - 150) + 150);
+	var x = Math.floor(random.get() * (window.innerHeight-200 - 150) + 150);
+	// var y = Math.floor(Math.random() * (window.innerWidth-100));
+	var y = Math.floor(random.get() * (window.innerWidth-100));
 
 	var mosquito = document.createElement('div');
 	mosquito.setAttribute('class', 'aedes');
 	mosquito.setAttribute('onMouseDown', 'killMosquito(this)');
-	mosquito.style.top = x + "px";
-	mosquito.style.left = y + "px";
+	mosquito.style.top = x;
+	mosquito.style.left = y;
 
 	gameZone.appendChild(mosquito);
 }
+
+function randomNumber() {
+	this.last = Date.now() % 100;
+
+	this.get = function() {
+		this.last = (this.last * 32719 + 3) % 32749;
+		return (this.last % 100) / 100;
+	}
+};
